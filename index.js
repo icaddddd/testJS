ad9b9f5304f8048b6c252f6f8a247370b88dea56
icad <rhisjaddjvtr@gmail.com>
2023-11-14 14:43:24 +0700

    first commit

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ function fizzBuzz(n) {
 }
 fizzBuzz(15);
 
-function lonelyinteger(a){
+function lonelyinteger(a) {
   for (let i = 0; i < a.length; i++) {
     if (a.indexOf(a[i]) === a.lastIndexOf(a[i])) {
       return a[i];
